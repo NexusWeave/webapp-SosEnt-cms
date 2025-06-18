a7bd893102865b8c46703916b4357708185f1603
Kristoffer <krigjo25@gmail.com>
2025-06-18 12:23:50 +0200

    Implement form generation logic in generateForm function

--- Prototype/static/js/controller.js
+++ Prototype/static/js/controller.js
@@ -6,3 +6,27 @@ function navigateView(view)
     model.app.currentPage = view;
     updateView();
 }
+
+function generateForm(id)
+{
+    console.log("Generating form for id:", id);
+    const form = model.forms[id];
+    const inputs = form.inputs;
+
+    let html = /*HTML*/ `
+        <form onsubmit="handleLogin(event)" method="${form.method}" class="${form.cls}">`;
+
+    for (let i = 0; i < inputs.length; i++)
+    {
+        const input = inputs[i];
+
+        html += /*HTML*/ `
+                <label for="${input.id}">${input.name}</label>
+                <input type="${input.type ? input.type : 'text'}" id="${input.id}" name="${input.name}" placeholder="${input.placeholder}" class="${input.cls}" required="${input.required ? 'required' : ''}">`;
+                
+    }
+    html += /*HTML*/ `
+    <button type="${form.btn.type}" class="${form.btn.cls}">${form.btn.name}</button>
+    </form>`;
+    return html;
+}
