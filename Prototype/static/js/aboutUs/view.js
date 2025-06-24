348262b231deea410a0a4cec9eb9be6c68c0161d
Kristoffer <krigjo25@gmail.com>
2025-06-24 17:45:30 +0200

    Refactor aboutUsView to improve data handling and enhance HTML structure; update newsSection and contactSection to utilize images correctly

--- Prototype/static/js/aboutUs/view.js
+++ Prototype/static/js/aboutUs/view.js
@@ -3,22 +3,26 @@ function aboutUsView(id)
 {
     //  Fetch the model
     const data = model.data;
-    const contact = data.contact;
-    const partners = data.partners;
+    const media = data.media;
     const nav = data.pages[id];
+    const contact = data.contact;
+    
+    const images = media.images;
+    const partners = images.partners;
+
+    const documents = media.files;
 
     const content = nav.content;
     const members = data.members;
-    const documents = content.documentation;
 
     let html = /*HTML*/ `
         ${header()}
         <main class = "${model.main.cls}">
             <section class="flex-wrap-row-justify-space-around">
-                ${newsSection(data.articlesBanner)}
+                ${newsSection(data.articles, images)}
                 ${aboutUsSection(content)}
                 ${basicDocuments(documents)}
-                ${contactSection(contact)}
+                ${contactSection(contact, images.contact)}
                 ${MemberContent(members)}
                 ${partnersSection(partners)}
             </section>
@@ -28,7 +32,7 @@ function aboutUsView(id)
         return html;
 }
 
-function newsSection(article)
+function newsSection(article, images)
 {
     let html = '';
     for (let i = 0; i < article.length; i++)
@@ -37,11 +41,11 @@ function newsSection(article)
             {
                 break;
             }
-
-            html += `
+            const image = findImage(article[i].category, images.news);
+            html += /*HTML*/`
                     <section class="news-container">
                         <article class = "flex-wrap-coumn news-article">
-                            <img class = "article-image" alt="${article[i].image.alt ? article[i].image.alt : null}" src="${article[i].image.src ? article[i].image.src : null}" />
+                            <img class = "${image.cls}" alt="${image.alt ? image.alt : null}" src="${image.src ? image.src : null}"/>
                             <h5 class = "h5">${article[i].headline}</h5>
                             <time datetime="${article[i].date}">${article[i].date}</time>
                             <a class="link" href="${article[i].link}">Read more</a>
@@ -78,14 +82,72 @@ function aboutUsSection(content)
     return html;
 }
 
+function basicDocuments(documents)
+{
+    let html = '';
+    html += `<section class="flex-wrap-row-justify-space-evenly">`;
+    
+    for (let i = 0; i < documents.length; i++)
+    {
+        const f = documents[i];
+        html += /*HTML*/`
+            <section class="${f.cls ? f.cls : "missing-class"}">
+                <h3>${f.name}</h3>
+                <p>Dato: ${f.date}</p>
+                <p>${f.description}</p>
+                <a href="${f.link}" download  rel="external">${f.name}</a>
+           </section>`;
+    }
+
+    html += `</section>`;
+    return html;
+}
+
+function contactSection(contact, images)
+{
+    const n = images.length;
+    let html = '<section class = "flex-wrap-row-justify-space-evenly">';
+    
+    for (let i = 0; i < n; i++)
+    {
+        const image = images[i];
+        const person = contact.content[i];
+        
+        html += /*HTML*/`
+            <section class="contact">
+                <section class="card bg-brown flex-row">
+                    <img src="${image.src ? image.src : null}" class="${image.cls}" alt="${image.alt ? image.alt : null}">
+                    <section class="card-body">
+                        <h3>${person.name}</h3>
+                        <h5 class="card-text h5">${person.title}</h5>
+                        <p class="card-text">
+                            Telefon: 
+                            <a class = "link" href="tel:" + ${person.phone}>
+                                ${person.phone}
+                            </a>
+                        </p>
+                        <p class="card-text">
+                            E-post :
+                            <a href="mailto:" + ${person.email}>
+                                Send en E-post
+                            </a>
+                        </p>
+                    </section>
+                </section>
+            </section>`;
+    }
+
+    return html;
+}
+
 function MemberContent(data)
 {
     const memberships = data.memberships;
 
         let html = /*HTML*/`
-    <section class = " flex-column">
-    <section class = "flex-wrap-column-justify-center">
-        <h3>${data.headline}</h3>`;
+        <section class = " flex-column">
+            <section class = "flex-wrap-column-justify-center">
+                <h3>${data.headline}</h3>`;
 
     if (data && memberships.length> 0)
     {
@@ -123,102 +185,24 @@ function MemberContent(data)
     html += /*HTML*/`</section></section>`;
     return html;
 }
-function constitutionContent(article)
-{
-    let html = /*HTML*/`
-    <section class = " bg-brown flex-wrap-row-align-items-center">`;
-
-    //  Vedtekter content
-    for (let i = 0; i < article.length; i++)
-    {
-        html += /*HTML*/`
-        <section class = "article-container">
-            <article>
-                
-                <h3>${article[i].headline}</h3>`;
-                
-                for (let j = 0; j < article[i].paragraphs.length; j++)
-                {
-                    html += /*HTML*/`
-                    <p>${article[i].paragraphs[j]}</p>`;
-                }
-
-            html += /*HTML*/`</article></section>`;
-        }
-        
-        html += /*HTML*/`</section>`;
-    return html;
-}
-
-function basicDocuments(documents)
-{
-    let html = '';
-    html += `<section class="flex-wrap-row-justify-space-evenly">`;
-    
-    for (let i = 0; i < documents.files.length; i++)
-    {
-        const f = documents.files[i];
-        html += `<section class="${f.cls ? f.cls : "missing-class"}">
-                    <h3>${f.name}</h3>
-                    <p>Dato: ${f.date}</p>
-                    <p>${f.description}</p>
-                    <a href="${f.link}" download  rel="external">${f.name}</a>
-                    </section>`;
-    }
-
-    html += `</section>`;
-    return html;
-}
-
-function contactSection(contact)
-{
-    let html = '<section class = "flex-wrap-row-justify-space-evenly">';
-    for (let i = 0; i < contact.content.length; i++)
-    {
-        const person = contact.content[i];
-         
-        html += /*HTML*/`
-            <section class="contact">
-                <section class="card bg-brown flex-row">
-                    <img src="${person.image.src ? person.image.src : null}" class="card-img-top" alt="${person.image.alt ? person.image.alt : null}">
-                    <section class="card-body">
-                        <h3>${person.name}</h3>
-                        <h5 class="card-text h5">${person.title}</h5>
-                        <p class="card-text">
-                            Telefon: 
-                            <a class = "link" href="tel:" + ${person.phone}>
-                                ${person.phone}
-                            </a>
-                        </p>
-                        <p class="card-text">
-                            E-post :
-                            <a href="mailto:" + ${person.email}>
-                                Send en E-post
-                            </a>
-                        </p>
-                    </section>
-                </section>
-            </section>`;
-    }
-
-    return html;
-}
 
 function partnersSection(partners)
 {
     let html = '<section class="flex-column">';
     html += '<h3>Våre samarbeids partnere</h3>';
 
-    if (partners && partners.length > 0)
+    const n = partners.length;
+
+    if (partners && n > 0)
     {
         html += '<section class="flex-wrap-row-justify-space-around partners-section">';
-        for (let i = 0; i < partners.length; i++)
+        for (let i = 0; i < n; i++)
         {
             const partner = partners[i];
             html += /*HTML*/`
                 <div class="partner">
                     <a href= ${partner.href}>
-                        <img src="${partner.logo.src}" alt="${partner.logo.alt}">
+                        <img src="${partner.src}" alt="${partner.alt}">
                     </a>
                 </div>`;
         }
@@ -229,9 +213,5 @@ function partnersSection(partners)
         html += '<p>No partners available.</p>';
     }
     return html;
-}
-function boardMembers(board)
-{
-    // Mermaid hierarchy chart?
 }
 
