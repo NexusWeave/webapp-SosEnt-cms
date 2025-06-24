a1583dec5eefb9db377df82343cc57f4b21b279a
Kristoffer <krigjo25@gmail.com>
2025-06-24 18:10:10 +0200

    Remove unused carouselView function from view.js

--- Prototype/static/js/memberPortal/view.js
+++ Prototype/static/js/memberPortal/view.js
@@ -35,52 +35,6 @@ function memberPortalView(id)
     }
 }
 
-function carouselView(images, carousel, content)
-{
-
-    let html = /*HTML*/`<section class = "${content.cls} flex-wrap-row-justify-center">
-    <div id="myCarousel" class="carousel slide" data-ride="carousel">
-         <ol class="carousel-indicators">`;
-
-    //  Indicators
-    for (let i = 0; i <= images.length; i++)
-    {
-        html += /*HTML*/`<li data-target="#myCarousel" data-slide-to="${i}"></li>`;
-    }
-
-    html += /*HTML*/`</ol>
-    <div class="carousel-inner">
-        <div class="item active">
-            <img alt="${carousel.alt ? carousel.alt : null}" src="${carousel.src ? carousel.src : null}">
-        </div>`;
-
-    //  Carousel images
-    for (let i = 0; i < images.length; i++)
-    {
-        const image = images[i];
-
-        html += `
-            <div class="item">
-                <img alt="${image.alt ? image.alt : null}" src="${image.src ? image.src : null}">
-            </div>`;
-    }
-
-    html += /*HTML*/`
-        </div>
-            <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-                <span class="glyphicon glyphicon-chevron-left"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="right carousel-control" href="#myCarousel" data-slide="next">
-                <span class="glyphicon glyphicon-chevron-right"></span>
-                <span class="sr-only">Next</span>
-            </a>
-        </div>`;
-
-    return html;
-
-}
-
 function memberPortalContent(data)
 {
     const archive = data.archive;
