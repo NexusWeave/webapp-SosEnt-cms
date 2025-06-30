deefd2f1d98e7680ff28df665e16fb04a44a9ed7
krigjo25 <krigjo25@gmail.com>
2025-06-30 16:17:37 +0200

    feat: update router configuration to include additional views

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '../App.vue';
+import NewsView from '../views/NewsView.vue';
+import HomeView from '../views/AboutView.vue';
+import programView from '../views/ProgramView.vue';
+import MembershipView from '../views/MembershipView.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +12,21 @@ const router = createRouter({
       path: '/',
       name: 'home',
       component: HomeView,
+    },
+    {
+      path: '/aktuelt',
+      name: 'news',
+      component: NewsView,
+    },
+    {
+      path: '/fordeler',
+      name: 'program',
+      component: programView,
+    },
+    {
+      path: '/medlemskap',
+      name: 'membership',
+      component: MembershipView,
     },
   ],
 })
