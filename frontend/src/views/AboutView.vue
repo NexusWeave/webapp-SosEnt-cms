e8ebf511cd6d36891c6d13d1fa2d37353bba7604
krigjo25 <krigjo25@gmail.com>
2025-06-30 16:18:16 +0200

    Inititalizing a new page

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -1,18 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+    <section>News</section>
+    <section>About Sosent</section>
+    <section>Files</section>
+    <section>Contact</section>
+    <section>Partners</section>
 </template>
-
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
 
 <script setup>
 
