f3328566e8535134c72aeac3e62cb893a7096d57
krigjo25 <krigjo25@gmail.com>
2025-07-02 13:49:04 +0200

    Refactor Link component template for improved structure and readability

--- frontend/src/components/navigation/Link.vue
+++ frontend/src/components/navigation/Link.vue
@@ -1,5 +1,23 @@
 <template>
-    <a
+    <a v-if="img && media.images.find(type => type === img.type)"
+        :href="data.href"
+        :target="isExternal ? '_blank': '_self'"
+        :rel="isExternal  && 'noopener noreferrer'">
+
+        <img :src="img.src" :alt="img.alt" :class="img.cls">
+    </a>
+    
+    <a v-if ="media.files.find(type => type === data.type)" 
+        :href="data.href" :class="data.cls" download>
+        {{ data.name }}
+    </a>
+    
+    <a v-if ="media.contactData.find(type => type === data.type)" 
+        :href="data.href" :class="data.cls">
+        {{ data.name }}
+    </a>
+
+    <a v-else
         :href="data.href"
         :class="data.cls"
         :target="isExternal ? '_blank': '_self'"
@@ -18,7 +36,14 @@ const props = defineProps({
     }
 });
 
+const media = 
+{
+    contactData: ['email', 'telephone'],
+    images: ['jpg', 'jpeg', 'png', 'svg'],
+    files: ['pdf', 'docx', 'xlsx', 'csv'],
+}
 const data = props.data;
+const img = data.img? data.img : null;
 const isExternal = data.type === 'external' || false;
 
 //console.log("Link component loaded with data: ", data, isExternal);
