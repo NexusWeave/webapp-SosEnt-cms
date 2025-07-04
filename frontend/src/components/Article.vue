55e9a15c4334ef450d56b2ae064d91da8e278e86
krigjo25 <krigjo25@gmail.com>
2025-07-04 15:53:47 +0200

    Add Article component to display articles with sorting and limiting functionality

---
+++ frontend/src/components/Article.vue
@@ -0,0 +1,31 @@
+<template>
+    <article v-for="article in articles" :key="article.id" :class="article.cls">
+        <img :src="article.img.src" :alt="article.img.alt" :class="article.img.cls">
+        <h2>{{ article.title }}</h2>
+        <p>{{ article.description }}</p>
+        <p class="date"><time datetime="{{ article.date }}">{{ article.date }}</time></p>
+        <a :href="article.href" class="read-more">Les mer</a>
+    </article>
+</template>
+<script setup>
+
+import { computed, defineProps } from 'vue';
+
+const n = 3;
+
+const props = defineProps({
+    data: {
+        type: Array,
+        required: true
+    }
+});
+
+const data = props.data;
+
+const articles = computed(() => {
+    data.sort((a, b) => new Date(b.date) - new Date(a.date));
+
+    return data.slice(0, n);});
+
+console.log("Articles Component :" + data);
+</script>
