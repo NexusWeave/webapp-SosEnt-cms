b370222cacf4e94b737346bd36609481f561ff56
krigjo25 <krigjo25@gmail.com>
2025-07-04 15:53:57 +0200

    Add Section component with props definition for data handling

---
+++ frontend/src/components/utils/Section.vue
@@ -0,0 +1,14 @@
+
+<template>
+    <section></section>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    data: {
+        type: Object,
+        required: false
+    }
+});
+</script>
