0de416e8478b2aac3bf31362f81c7165f33c2d18
krigjo25 <krigjo25@gmail.com>
2025-07-09 19:06:51 +0200

    Add Image component for dynamic image rendering, and remove unused components

---
+++ frontend/src/components/icons/Image.vue
@@ -0,0 +1,15 @@
+<template>
+    <img :src="data.src" :alt="data.alt" :class="data.cls">
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true
+    }
+});
+
+const data = props.data;
+</script>
