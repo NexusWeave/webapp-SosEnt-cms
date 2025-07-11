5049935eb9fd7033a5d7513bc04dd18e1b4d17e2
krigjo25 <krigjo25@gmail.com>
2025-07-11 13:32:41 +0200

    Refactor Anchor component; improve media type handling and enhance external link logic for better clarity and functionality

--- frontend/src/components/navigation/Anchor.vue
+++ frontend/src/components/navigation/Anchor.vue
@@ -1,18 +1,19 @@
 <template>
-    <a
+    <a v-if ="data.href"
         :href="data.href"
-        class=""
-        :target="isExternal ? '_blank': '_self'"
-        :rel="isExternal ? 'noopener noreferrer' : null" 
-        :download="media.files.find(type => type === data.type) ? 'SosEnT_'+ data.name.replace(/\s+/g, '_').toLowerCase() + '.' + data.type : null"
+        :class="data.cls"
+        :data-external-link="isExternal()? 'true' : 'false' "
+        :target="isExternal() ? '_blank' : '_self'"
+        :rel="isExternal() ? 'noopener noreferrer' : null"
+        :download="isMedia() == 'download' ? 'SosEnT_'+ data.name.replace(/\s+/g, '_').toLowerCase() + '.' + data.type[0] : null"
         >
 
-         <template v-if="img && media.images.find(type => type === img.type)">
+         <template v-if="img && isMedia() == 'img'">
             <Img :data="img"/>
         </template>
 
-        <template v-if="media.files.find(type => type === data.type)">
-            <h3 :class="data.type">
+        <template v-if="isMedia() =='file'">
+            <h3 :class="data.type[0]">
                 <span class="icon" :aria-label="data.name"></span> 
                 {{ data.name }}
             </h3>
@@ -39,15 +40,61 @@
 
     const media = 
     {
-        contactData: ['email', 'telephone'],
         images: ['jpg', 'jpeg', 'png', 'svg'],
-        files: ['pdf', 'docx', 'xlsx', 'csv'],
+        files: ['pdf'],
+        downloadFiles: ['docx', 'xlsx', 'csv'],
     }
 
     const data = props.data;
     const img = data.img? data.img : null;
-    const isExternal = data.type === 'external' || false;
+    
+    const isExternal = () => {
+        if (Array.isArray(data.type)) {
+            return data.type.find(type => type === 'external');
+            
+        }
+        else {
+            return data.type == 'external';
+        }
+    };
 
-    //console.log("Link component loaded with data: ", data, isExternal);
+    console.log("Link component loaded with data: ", data, isExternal());
+    
+    const isMedia = () => {
+        if (!data.type) return false;
 
+        if (Array.isArray(data.type)) {
+            switch (data.type[0]) {
+                case media.files.find(type => type === data.type[0]):
+                    return 'file';
+                
+                case media.downloadFiles.find(type => type === data.type[0]):
+                    return 'download';
+
+                case media.images.find(type => type === data.type[0]):
+                    return 'img';
+
+                default:
+                    return false;
+            }
+            
+        }
+        else
+        {
+            switch (data.type)
+            {
+                case media.files.find(type => type === data.type):
+                    return 'file';
+                
+                case media.downloadFiles.find(type => type === data.type):
+                    return 'download';
+
+                case media.images.find(type => type === data.type):
+                    return 'img';
+
+                default:
+                    return false;
+            }
+        }
+    };
 </script>
