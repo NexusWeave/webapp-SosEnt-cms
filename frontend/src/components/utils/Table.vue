03aa92b31b2685f0e47cd1fd40a3043e7d5bbc5e
krigjo25 <krigjo25@gmail.com>
2025-07-17 17:52:10 +0200

    Add Table component for dynamic data rendering with anchor and date support

---
+++ frontend/src/components/utils/Table.vue
@@ -0,0 +1,38 @@
+<template>
+    <table :class="data.cls">
+        <thead v-for="row in data.rows">
+            <tr>
+                <th>{{ row.title }}</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+
+                <td v-if="data.anchor">
+                    <Link :data="data.anchor" />
+                </td>
+
+                <td v-if="data.date">
+                    <time :datetime="data.date">{{ data.date }}</time>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
+
+<script setup>
+
+    import { defineProps } from 'vue';
+
+    import Link from '@/components/navigation/Anchor.vue';
+
+    const props = defineProps({
+        data: {
+            type: Object,
+            required: true
+        },
+    });
+
+    const data = props.data;
+    console.log("Table Component loaded with data: ", data);
+</script>
