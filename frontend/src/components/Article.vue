802281fffb021e523c205df8c7a714a0960776c2
krigjo25 <krigjo25@gmail.com>
2025-07-17 17:52:23 +0200

    Refactor Article component to improve structure and data handling

--- frontend/src/components/Article.vue
+++ frontend/src/components/Article.vue
@@ -1,65 +1,60 @@
 <template>
-    <article v-for="article in articles" :key="article.id" :class="article.cls">
-        <header>
+    <article v-if="article" :class="article.cls">
+        <header >
             <Figure :data="article.img" v-if="article.img" />
             <h2>{{ article.title }}</h2>
             <p>
                 <span class="date" v-if="article.date">
                     <time datetime="{{ article.date }}">{{ article.date }}</time>
                 </span>
-                <span v-if="article.anchor" class="anchor">
+                <span v-if="article.anchor && !article.footer" class="anchor">
                     <Anchor :data="article.anchor"/>
                 </span>
             </p>
+            <p v-if="article.author" class="author">
+                <span v-for="(author, index) in article.author" :key="index">{{ author }}</span>
+            </p>
+            <p v-if="!article.footer" v-for="tag in article.tags" :key="tag.id" :class="tag.cls">{{ tag.title }}</p>
         </header>
-        
-        <p v-if="ingress" :class='ingress.cls'>{{ ingress.content }}</p>
-        
-        <section v-if="sections > 0" v-for="section in sections" :key="section.id" :class="section.cls">
-            
-            <h2>{{ section.title }}</h2>
-            <p v-for="paragraph in section.paragraphs" :key="paragraph.id">{{ paragraph.content }}</p>
-            <ul v-if="section.list">
-                <li v-for="item in section.list.items" :key="item.id">{{ item.content }}</li>
-            </ul>
-            <p v-if="section.conclusion">{{ section.conclusion }}</p>
+        <main v-if="article.content">
+            <p v-if="article.ingress" :class='article.ingress.cls'>{{ article.ingress.content }}</p>
 
-            <Anchor  :data="section.anchor? section.anchor: null" />
-            <a v-if="article.href" :href="article.href" class="read-more">Les mer</a>
-        </section>
-        
-        <footer v-if="footer" :class="footer.cls">
-            <p>{{footer.content}}</p>
+            <section v-if="article.sections > 0" v-for="section in article.sections" :key="section.id" :class="section.cls">
+
+                <h2>{{ section.title }}</h2>
+                <p v-for="paragraph in section.paragraphs" :key="paragraph.id">{{ paragraph.content }}</p>
+                <ul v-if="section.list">
+                    <li v-for="item in section.list.items" :key="item.id">{{ item.content }}</li>
+                </ul>
+                <p v-if="section.conclusion">{{ section.conclusion }}</p>
+
+                <Anchor  :data="section.anchor? section.anchor: null" />
+            </section>
+        </main>
+
+        <footer v-if="article.footer" :class="article.footer.cls">
+            <p>{{article.footer.content}}</p>
         </footer>
     </article>
 </template>
 
 <script setup>
 
-    import { computed, defineProps } from 'vue';
+    import { defineProps } from 'vue';
     import Anchor from './navigation/Anchor.vue';
     import Figure from '@/components/media/Figure.vue';
-    
-    const n = 3;
 
     const props = defineProps({
         data: {
-            type: Array,
+            type: Object || Array,
             required: true
         }
     });
 
-    const data = props.data;
+    const article = props.data;
+    console.log("Articles Component :", article);
 
-    const articles = computed(() => {
-        data.sort((a, b) => new Date(b.date) - new Date(a.date));
+    //console.log("Article Component loaded with data: ", props.data);
 
-        return data.slice(0, n);}
-    );
-
-    const sections = articles.sections ? articles.sections : null;
-    const footer = articles.footer ? articles.footer : null;
-    const ingress = articles.ingress ? articles.ingress : null;
-
-    console.log("Articles Component :" + data);
+    
 </script>
