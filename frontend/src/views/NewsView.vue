673a053147839eb4ae29b4dd972b27503942683a
krigjo25 <krigjo25@gmail.com>
2025-07-18 18:22:32 +0200

    Refactor Articles rendering in NewsView for improved data handling and consistency

--- frontend/src/views/NewsView.vue
+++ frontend/src/views/NewsView.vue
@@ -1,8 +1,7 @@
 <template v-if="news.isLoaded">
     <h2>Recent News</h2>
-    <section :class="news.data.cls[1]" v-if="news.recent.length > 0">
-        <Articles v-for="article in news.recent" :key="article.id" :data="{ head: article.head }" />
-    </section>
+    <Articles v-for="article in news.recent" :key="article.id" :data="article" v-if="news.recent.length > 0"/>
+
     <h2 v-if="news.archived.length > 0">Archived News</h2>
     <section :class="news.data.cls[1]" v-if="news.archived.length > 0">
         <Table v-for="article in news.archived" :key="article.id" :data="article" />
