6b99d3b2d4753f9a85b1e5915671e45b04125730
krigjo25 <krigjo25@gmail.com>
2025-07-22 20:54:44 +0200

    Refactor NewsView.vue to update headings and replace Card component with NewsCard for consistency

--- frontend/src/views/NewsView.vue
+++ frontend/src/views/NewsView.vue
@@ -1,10 +1,21 @@
 <template v-if="news.isLoaded">
-    <h2>Recent News</h2>
-    <Articles v-for="article in news.recent" :key="article.id" :data="article" v-if="news.recent.length > 0"/>
+    <h2>Nyheter</h2>
+    <Articles 
+        v-if="news.recent.length > 0"
+        v-for="article in news.recent" 
+        :key="article.id" 
+        :data="article"/>
 
-    <h2 v-if="news.archived.length > 0">Arkiverte Nyheter</h2>
-    <section :class="news.data.cls[1]" v-if="news.archived.length > 0">
-        <Card v-for="article in news.archived" :key="article.id" :data="article" />
+    <h2 v-if="news.archived.length > 0"> Arkiverte Nyheter</h2>
+    <section :class="[news.data.cls[1]]" v-if="news.archived.length > 0">
+            <NewsCard 
+                v-for="article in news.archived" 
+                :key="article.id" 
+                :data="article" 
+                :Cls="[ '', '', article.cls[2], article.cls[3],
+                article.cls[8], article.cls[1], article.cls[6],
+                '', article.cls[14]
+                ]"/>
     </section>
 </template>
 
@@ -13,7 +24,7 @@
     import { onMounted } from 'vue';
 
     import Articles from '@/components/article/Article.vue';
-    import Card from '@/components/utils/News-Card.vue';
+    import NewsCard from '@/components/article/Article-Header.vue';
     
     import { newsStore } from '@/stores/newsStore.js';
     
