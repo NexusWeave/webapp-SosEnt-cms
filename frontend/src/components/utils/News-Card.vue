836e845d55f566fcd3b548feef44402ea5ca7ae7
krigjo25 <krigjo25@gmail.com>
2025-07-22 15:55:43 +0200

    Add News-Card component and remove Table component for improved structure

---
+++ frontend/src/components/utils/News-Card.vue
@@ -0,0 +1,36 @@
+<template>
+    <article :class="[article.cls[0], article.cls[1]]">
+        <header>
+            <h2 :class="article.cls[2]">
+                {{ article.title }}
+            </h2>
+            <section :class="article.cls[7]">
+                <MetaData v-if="article.date && article.date.type == 'updated'" :data="article.date" :Cls="article.date.cls" :text="article.date.type"/>
+
+                <p>{{ article.ingress.content }}</p>
+                            
+                <p>
+                    <MetaData v-if="article.tags" :array="article.tags" />
+                    <Btn v-if="article.anchor && article.section" :data="article.anchor" />
+                </p>
+            </section>
+        </header>
+    </article>
+</template>
+
+<script setup>
+    import { defineProps, reactive } from 'vue';
+
+    import MetaData from '@/components/utils/Span.vue';
+    import Btn from '@/components/navigation/Button.vue';
+
+    const props = defineProps({
+        data: {
+            type: Object,
+            required: true
+        },
+    });
+
+    const article = reactive(props.data);
+    console.log("News Card Component loaded with data: ", article);
+</script>
