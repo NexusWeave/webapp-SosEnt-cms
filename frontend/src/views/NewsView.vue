747f3ede15ad0b12e9c44da59c798d4028f61a73
krigjo25 <krigjo25@gmail.com>
2025-07-23 12:52:37 +0200

    Refactor NewsView.vue to improve archived articles display and pagination logic

--- frontend/src/views/NewsView.vue
+++ frontend/src/views/NewsView.vue
@@ -3,34 +3,62 @@
     <Articles 
         v-if="news.recent.length > 0"
         v-for="article in news.recent" 
-        :key="article.id" 
-        :data="article"/>
+        
+        :data="article"
+        :key="article.id" />
 
     <h2 v-if="news.archived.length > 0"> Arkiverte Nyheter</h2>
+    Display 3 archived articles
+    <Pagnition 
+        :Cls="archived.cls"
+        :totalPage="archived.total" 
+        :activePage="archived.activePage"
+        @update="(page) => archived.activePage = page" />
+
     <section :class="[news.data.cls[1]]" v-if="news.archived.length > 0">
             <Articles 
-                v-for="article in news.archived" 
+                v-for="article in archived.articles" 
                 :key="article.id" 
                 :data="article" 
                 :Cls="[ '', '', article.cls[2], article.cls[3],
                 article.cls[8], article.cls[1], article.cls[6],
                 '', article.cls[14]
-                ]"
-                />"
+                ]"/>
     </section>
 </template>
 
 <script setup>
 
-    import { onMounted } from 'vue';
+    import { newsStore } from '@/stores/newsStore.js';
+    import { computed, reactive, onMounted } from 'vue';
 
     import Articles from '@/components/article/Article.vue';
-    import NewsCard from '@/components/article/Article-Header.vue';
-    
-    import { newsStore } from '@/stores/newsStore.js';
+
+
+import Pagnition from '@/components/utils/Pagnition.vue';
     
 
     const news = newsStore();
+
+
+    const archived = reactive(
+        {
+            n: 3,
+            activePage: 1,
+            cls: ['flex-wrap-row-justify-center', 'archive-pagnition'],
+            total : computed(() => Math.ceil(news.archived.length / archived.n) + 2),
+            articles: computed(() =>
+            {
+                const n = archived.n;
+                const current = archived.activePage;
+                
+                const end = (current * n);
+                const start = (current - 1) * n;
+
+                return news.archived.slice(start, end);
+            })
+        }
+    );
 
     onMounted( async () => {
         await news.fetchNews();   
