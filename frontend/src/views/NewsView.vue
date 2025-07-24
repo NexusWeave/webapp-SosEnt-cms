5b237c4952e0ae184ca4e2870c03ee42e29e905f
krigjo25 <krigjo25@gmail.com>
2025-07-24 19:19:43 +0200

    Refactor NewsView.vue to enhance class binding, improve section titles, and fix minor typos in the template structure

--- frontend/src/views/NewsView.vue
+++ frontend/src/views/NewsView.vue
@@ -1,14 +1,16 @@
 <template v-if="news.isLoaded">
-    <h2>Nyheter</h2>
+    <h2 :class="page.cls">{{ page.title}}</h2>
     <Articles 
         v-if="news.recent.length > 0"
-        v-for="article in news.recent" 
-        
+        v-for="article in news.recent"         
         :data="article"
-        :key="article.id" />
+        :key="article.id" 
+        :Cls="[ article.id % 2 ? article.cls[1] : article.cls[2],
+                article.cls[10], article.cls[3], article.cls[11],
+                article.cls[8], article.cls[8]]"/>
 
-    <seaction v-if="news.archived.length > 0">
-        <h2> Arkiverte Nyheter</h2>
+    <section v-if="news.archived.length > 0">
+        <h2 :class="page.section.cls"> {{ page.section.title }}</h2>
         <Pagnition
             :Cls="archived.cls"
             :totalPage="archived.total" 
@@ -16,37 +18,41 @@
             @update="(page) => archived.activePage = page" />
     
         <section :class="[news.data.cls[1]]" v-if="news.archived.length > 0">
-                <Articles 
-                    v-for="article in archived.articles" 
-                    :key="article.id" 
-                    :data="article" 
-                    :Cls="[ '', '', article.cls[2], article.cls[3],
-                    article.cls[8], article.cls[1], article.cls[6],
-                    '', article.cls[14]
-                    ]"/>
+            <Articles 
+                v-for="article in archived.articles" :key="article.id"
+                :data="article" 
+                :Cls="[[article.cls[0], article.cls[12]], article.cls[12], 
+                    article.cls[3], article.cls[11], article.cls[8],
+                    article.cls[6]]"/>
         </section>
-    </seaction>
+    </section>
 </template>
 
 <script setup>
 
-    import { newsStore } from '@/stores/newsStore.js';
+    import { newsStore } from '@/stores/news-store.js';
     import { computed, reactive, onMounted } from 'vue';
 
     import Articles from '@/components/article/Article.vue';
-
-
-import Pagnition from '@/components/utils/Pagnition.vue';
+    import Pagnition from '@/components/utils/Pagnition.vue';
     
-
     const news = newsStore();
 
-
+    const page = reactive(
+        {
+            title: 'Siste nytt fra foreningen',
+            cls: ['flex-wrap-row-justify-center'],
+            section: {
+                title: 'Nyhetsarkiv',
+                cls: ['flex-wrap-row-justify-center']
+            }
+        }
+    );
     const archived = reactive(
         {
             n: 3,
             activePage: 1,
-            cls: ['flex-wrap-row-justify-center', 'archive-pagnition'],
+            cls: ['flex-wrap-row-justify-center', 'flex-wrap-row-align-items-center'],
             total : computed(() => Math.ceil(news.archived.length / archived.n)),
             articles: computed(() =>
             {
