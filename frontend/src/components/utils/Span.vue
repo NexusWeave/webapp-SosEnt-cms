6cc11308846d7d5d6ab9dc6e9ee96272cd449a18
krigjo25 <krigjo25@gmail.com>
2025-07-25 18:01:44 +0200

    Refactor Span component to enhance date handling and streamline template structure

--- frontend/src/components/utils/Span.vue
+++ frontend/src/components/utils/Span.vue
@@ -1,12 +1,22 @@
 <template>
     <template v-if="data">
-        <span :class="cls" v-if="text">
-            {{ dateText() }} <b> * </b>
-            <time :datetime="date()">
-                <b>{{ date() }}</b>
+
+        <span :class="cls" v-if="dateObject.isLoaded()">
+            {{ dateObject.text() }}
+            
+            <b>{{ dateObject.delimiter }}</b>
+            
+            <time :datetime="dateObject.updated() ? dateObject.updated() : dateObject.published()">
+                <b>{{ dateObject.updated() ? dateObject.updated() : dateObject.published() }}</b>
             </time>
+            <template v-if="dateObject.icon.exists()">
+                <span :class="dateObject.icon.cls[0]">
+                    <span :class="dateObject.icon.cls[1]"></span>
+                </span>
+            </template>
         </span>
-        <span :class="cls" v-else-if="data.anchor">
+
+        <span :class="cls" v-if="data.anchor">
             <Anchor :data="data"/>
         </span>
     </template>
@@ -45,29 +55,32 @@
             type: Array,
             required: false
         },
-        text: {
-            type: String,
-            required: false
-        },
 
     });
 
     const cls = props.Cls;
     const data = props.data;
-    const text = props.text;
     
-    const date = () => 
+    const dateObject =
     {
-        return text == 'updated' ? new Date(data.updated).toLocaleDateString()
-            : text == 'published' ? new Date(data.published).toLocaleDateString()
-            : null;
-    };
-    const dateText = () => 
-    {
-        return text == 'updated' ? 'Oppdatert '
-            : text == 'published' ? 'Publisert '
-            : null;
-    };
+        delimiter: '*',
+        isLoaded: () => { if(data) return true; else return false; },
+        type: () => { if (data.type) return data.type; else return 'date'; },
+        icon:
+        {
+            cls: ['calendar', 'icon'],
+            exists : () => { if (data.icon) return true; else return false; }
+            
+        },
+        text : () => 
+        { 
+            return data.updated ? 'Oppdatert '
+            : data.published ? 'Publisert '
+            : ''; 
+        },
+        updated: () => data.updated ? new Date(data.updated).toLocaleDateString() : null,
+        published: () => data.published ? new Date(data.published).toLocaleDateString() : null,
 
+    }
     //console.log("Span Component loaded with data: ", dateText(), props.text, data);
 </script>
