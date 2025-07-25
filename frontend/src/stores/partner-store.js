2f2c769ad62a5a9e2cb0f03f85020bccbbe6e7df
krigjo25 <krigjo25@gmail.com>
2025-07-25 10:34:02 +0200

    Refactor partner store to remove unused properties and enhance partner addition logic

--- frontend/src/stores/partner-store.js
+++ frontend/src/stores/partner-store.js
@@ -1,23 +1,50 @@
 //  This file is a part of the SoSEnT web application project.
-import { reactive } from 'vue';
 import { defineStore } from 'pinia';
-
 import { fetchPartners } from '@/services/sosent-partners-api.js';
 
 export const partnerStore = defineStore('partnerData',
     {
         state: () => ({
             data: {
-                title: 'Media',
-                cls: ['flex-column', 'flex-wrap-row-justify-space-evenly'],
-                items: [],
+                partners: [],
                 isLoaded: false,
+                
             },
+            
         }),
         actions:
         {
+            addPartner(partners)
+            {
+                partners.forEach(partner =>
+                    {
+                        partner.cls = ['partner-container', 'flex-wrap-row-justify-space-evenly']
+                        this.data.partners.push(partner);
+                        console.log("Partner added: ", partner);
+                    }); 
+                    
+            },
+            fetchPartners()
+            {
+                if (this.data.isLoaded)  return;
+
+                fetchPartners().then((partners) =>
+                    {
+                        this.data.isLoaded = true;
+                        this.addPartner(partners);
+
+                    }). catch((error) =>
+                    {
+                        this.data.isLoaded = false;
+                        console.warn("Partners Not found:", error);
+                        
+                    });
+            }
         },
         getters:
         {
+            partners: (state) => {return state.data.partners},
+            isLoaded: (state) => {return state.data.isLoaded},
+
         },
     });
