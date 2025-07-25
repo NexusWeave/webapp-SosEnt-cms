e30d3fac6b9647fe4f3064c8dc990472df015ce3
krigjo25 <krigjo25@gmail.com>
2025-07-25 18:02:05 +0200

    Refactor organization data structure to enhance media and team member details

--- frontend/src/services/sosent-organization-api.js
+++ frontend/src/services/sosent-organization-api.js
@@ -1,13 +1,110 @@
 //  This file is a part of the SoSEnT web application project.
 import {reactive } from 'vue';
-import { fetchOrganization } from '@/services/sosent-organization-api.js';
+import { generateHexID } from '@/utils/utils';
 
 
-const organization = reactive();
+const organization = reactive(
+    {
+        media:
+        [
+            {
+                id: generateHexID(),
+                date: {published: '01.04-2025'},
+                description: 'Organisasjonskart over styret i SosEnt Norge.',
+                anchor:
+                {
+                    type: ['pdf'],
+                    label: 'Organisasjonskart',
+                    href: '/media/files/organization/SosEnt-Norge-organisajonskart.pdf',
+                },
+            },
+            {
+                id: generateHexID(),
+                date: {published: '01.04-2025'},
+                description: 'Vedtekter for medlemmene i SosEnt Norge.',
+                anchor:
+                {
+                    type: ['pdf'],
+                    label: 'Vedtekter',
+                    href: '/media/files/organization/SosEnt-Norge-vedtekter.pdf',
+                },
+            },
 
-export const fetchOrganization = async () =>
+        ],
+        team:
+        [
+            {
+                id: generateHexID(),
+                title: 'Styreleder',
+                name: 'Helle V. Rødahl',
+            // Make the email and phone numbers bot protected
+            contactInfo:
+            [
+                {
+                    id: 0,
+                    anchor:
+                    {
+                        href: 'mailto:' + 'demo' +'@' + 'example.com',
+                    },
+                },
+                {
+                    id: 1,
+                    anchor:
+                    {
+                        href : 'tel:' + '+47' + '12' + '34' + '56' + '78',
+                    },
+                },
+            ],
+
+            img:
+            {
+                type: 'jpg',
+                cls: ['contact-logo'],
+                alt: "JPG bilde av Helle V. Rødahl",
+                src: '/media/images/team/profil-helle-privat.jpg',
+            },
+        },
+        {
+            id: generateHexID(),
+            name: 'Rune J. Kvarme',
+            title: 'Generalsekretær',
+
+            contactInfo:
+            [
+                {
+                    id: 0,
+                    anchor:
+                    {
+                        label: 'Send en Epost',
+                        href: 'mailto:' + 'rune.kvarme' + '@' + 'samfunnsbedriftene.no',
+                    },
+                },
+                {
+                    id: 1,
+                    anchor:
+                    {
+                        label: '+47' + '12' + '34' + '56' + '78',
+                        href : 'tel:' + '+47' + '12' + '34' + '56' + '78',    
+                    },
+                    
+                },
+            ],
+
+            img:
+            {
+                type: 'png',
+                cls: ['card-img'],
+                alt: "Rune J. Kvarme ",
+                src: '/media/images/team/52700234381_5e75074f92_q.jpg',
+            },
+        },
+        ],
+    });
+
+export const fetchData = async () =>
 {
     return new Promise(resolve => {
-        setTimeout(() => {resolve(organization);}, 10);
+        setTimeout(() => {
+            resolve(organization);}, 10);
     })
 }
