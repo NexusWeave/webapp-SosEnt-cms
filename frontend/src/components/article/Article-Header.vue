cbaae16b376dcf636f06e80b944a580b008d7e2b
krigjo25 <krigjo25@gmail.com>
2025-07-28 18:43:51 +0200

    Refactor Article-Header.vue for improved conditional rendering and code clarity

--- frontend/src/components/article/Article-Header.vue
+++ frontend/src/components/article/Article-Header.vue
@@ -1,22 +1,24 @@
 <template>
     <section :class="cls[0]">
         <section :class="cls[1]">
-            <Figure  v-if="!isNewsPage || article.archived ":data="article.img"/>
+            <Figure  v-if="!isArticlePage && !isNewsPage ||  article.archived" :data="article.img"/>
+            <Anchor  v-if="isArticlePage" :data="anchor"/>
             <h2 :class="cls[2]"> {{ article.title }}</h2>
-            <p :class="cls[3]">
+            <p>
                 <MetaData 
                     :data="article.date" 
-                    :Cls="[article.date.cls]" />
-                <span :class="cls[4]" v-if="article.tags">
+                    Cls="article-date" />
+                
+                <span :class="cls[3]" v-if="article.tags">
                     <MetaData v-if="article.tags" 
                         :array="article.tags"/>
                 </span>
             </p>
-            <p :class="cls[3]">{{ article.ingress.content }}</p>
-            <Btn
-                v-if="article.btn  && article.section && !isArticlePage"
-                :data="article.btn"/>
+            <p>{{ article.ingress.content }}</p>
+            <Anchor v-if="!isArticlePage && article.section"
+                :data="anchor"/>
 
+            <Figure  v-if="isArticlePage" :data="article.img"/>
         </section>
         <section :class="cls[1]" v-if="isNewsPage && !isArticlePage && !article.archived">
             <Figure :data="article.img"/>
@@ -26,11 +28,12 @@
 
 <script setup>
 
-    import { defineProps } from 'vue';
+    import { computed, defineProps, reactive } from 'vue';
 
     import Figure from '@/components/media/Figure.vue';
     import MetaData from '@/components/utils/Span.vue';
     import Btn from '@/components/navigation/Button.vue';
+    import Anchor from '@/components/navigation/Anchor.vue';
 
     const props = defineProps({
         article: {
@@ -55,6 +58,14 @@
     const article = props.article;
     const isNewsPage = props.isNewsPage;
     const isArticlePage = props.isArticlePage;
+
+    const anchor = computed(() => {
+        return isArticlePage ? {
+            cls: ['button', 'read-more'],
+            label: 'Gå tilbake',
+            href: () => { window.history.back() }
+        } : article.anchor;
+    });
 
     //console.log("Article Header Component :", article, isNewsPage, isArticlePage);
 </script>
