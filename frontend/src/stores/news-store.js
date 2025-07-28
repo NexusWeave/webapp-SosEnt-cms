add627aef82d2180cc93658953024083f1459772
krigjo25 <krigjo25@gmail.com>
2025-07-28 18:42:52 +0200

    Refactor article styling and simplify anchor properties for improved clarity

--- frontend/src/stores/news-store.js
+++ frontend/src/stores/news-store.js
@@ -22,63 +22,28 @@ export const newsStore = defineStore('newsData',
                     article.archived = false;
 
                     article.cls = 
-                    [
-                        // 0 1 2
-                        'flex-column-align-items-center', 'flex-wrap-row',
-                        'flex-row-reversed-justify-center-align-content-center',
-                        // 3 4 5
-                        'article-title-h2', 'article-title-h3', 'article-title-h4',
-                        // 6 7 8 9
-                        'news-card', 'news-article', 'tags', 'author',
-                        //10 11 12
-                        'article-ingress', 'text', 'article-section',
-                        //13 14
-                        'article-section-content', 'article-conclusion',
-                        //15 16
-                        'article-citation','article-head-content',
-                        //17 18 19 20 21
-                        'container-grid', 'main-grid', 'aside-grid',
-                        'component-theme','article-cta'];
+                    ['article-title-h2', 'tags'];
 
-                    // Tags
-                    /*article.tags.forEach(tag => { 
-                        tag.cls = ['news-tag'];
-                        tag.anchor = { href: '#' };
-                    });*/
-                    
+                    if (article.tags)
+                    {
+                        article.tags.forEach(tag => {
+                            tag.cls = ['news-tag'];
+                            tag.anchor = { href: '#' };
+                        });
+                    }
+
                     article.anchor = 
                     {
                         label: 'Les mer',
-                        cls: ['read-more'],
+                        cls: ['button','read-more'],
                         href: '#/aktuelt/artikkel/' + article.id
-                    };
-                    
-                    article.btn = 
-                    {
-                        anchor: article.anchor,
-                        text: article.anchor.name,
-                        href: article.anchor.href,
-                        cls: ['button', 'read-more'],
-                    };
-
-                    const section = article.section;
-                    section.btn = 
-                    {
-                        anchor: article.anchor,
-                        text: article.anchor.name,
-                        href: article.anchor.href,
-                        cls: ['button', 'read-more'],
-                    };
+                    };  
 
                     const date = article.date;
                     date.type = 'news';
-                    date.cls = ['article-date'];
 
-                    const contents = section.contents;
+                    const contents = article.section.contents;
                     contents.forEach((content) => {
-                        content.cls = ['flex-wrap-row', 'flex-wrap-reversed-row',
-                                        'article-content'];
-
                         const figure = content.img;
                         if (figure)
                         {
@@ -90,15 +55,15 @@ export const newsStore = defineStore('newsData',
                         {
                             cta.forEach((item, i) =>
                             {
-                                item.cls = ['cta-section', 'cta-content'];
-                                if (item.nav > 0)
+                                if (item.anchor > 0)
                                 {
-                                    const nav = item.nav;
-                                    nav.forEach((navItem, j) => {
-                                        navItem.anchor.href.includes('/media/documents/') ?
-                                        navItem.anchor.cls = ['pdf', 'nav-link'] :
-                                        navItem.anchor.cls = ['nav-link', 'cta-content'];
+                                    const anchor = item.anchor;
+                                    anchor.forEach((anchorItem, j) => {
+                                        anchorItem.href.startsWith('/media/documents/') ?
+                                        anchorItem.cls = ['pdf', 'nav-link'] :
+                                        anchorItem.cls = ['nav-link', 'cta-content'];
                                     });
+                                    
                                 }
                             });
                         }
