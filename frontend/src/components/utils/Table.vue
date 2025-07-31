3973884f8d22d90973b528762c003e472ffa20d4
krigjo25 <krigjo25@gmail.com>
2025-07-31 16:47:23 +0200

    Refactor: Add Table component with member display and computed properties

---
+++ frontend/src/components/utils/Table.vue
@@ -0,0 +1,48 @@
+<template>
+    <template v-if="hasMembers">
+        <table>
+            <tbody>
+                <tr v-for="member in members.slice(0, 8)" :key="member.id">
+                    <td v-if="member.anchor">
+                        <Anchor :data="member.anchor"/>
+                    </td>
+                    <td v-else>
+                    {{ member.label }}
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </template>
+
+</template>
+
+<script setup>
+    import { computed, defineProps } from 'vue';
+
+    import Anchor from '@/components/navigation/Anchor.vue';
+
+    const props = defineProps({
+        data: {
+            type: [Object, Array],
+            required: true
+        },
+        cls: {
+            type: Array,
+            required: false
+        }
+    });
+
+
+    const data = props.data;
+    const cls = props.cls ?? [];
+
+    const hasMembers = computed(() =>
+    {
+        console.log('hasMembers', members);
+        return !!data.members && data.members.length > 0;
+    });
+    const members = computed(() => {
+        return !!hasMembers.value ? data.members : [];
+    });
+    
+    </script>
