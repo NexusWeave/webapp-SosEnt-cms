36aa00b6bf50f0e84574b02db190ab74fc492166
krigjo25 <krigjo25@gmail.com>
2025-07-31 16:45:57 +0200

    Refactor: Update mediaContent structure for improved data handling in API simulation

--- frontend/src/services/sosent-media-api.js
+++ frontend/src/services/sosent-media-api.js
@@ -4,10 +4,73 @@ import {reactive } from 'vue';
 import { generateHexID } from '@/utils/utils.js';
 
 // API simulation
-const mediaContent = reactive();
+const mediaContent = {
+    media:
+    [
+        {
+            type: 'pdf',
+            id: generateHexID(),
+            label: 'organisasjonens Kart',
+            date: {published: '01.04-2025'},
+            name: 'SosEnt-Norge-organisajonskart',
+            description: 'organisasjonenskart over styret i foreningen.',
+
+            tag:
+            [
+                'organisasjonen', 'dokumenter', 'medlemskap', 'pdf',
+                'organisasjonskart'
+            ],
+        },
+        {
+            type: 'pdf',
+            label: 'Vedtekter',
+            id: generateHexID(),
+            name: 'SosEnt-Norge-Vedtekter',
+            date: {published: '01.04-2025'},
+            description: 'Vedtekter for medlemmene i foreningen.',
+
+            tag: 
+            [
+                'organisasjonen', 'dokumenter', 'medlemskap', 'pdf',
+                'vedtekter'
+            ],
+            
+        },
+        {
+            type: 'pdf',
+            id: generateHexID(),
+            label: 'Grunnlags Dokument',
+            date: {published: '02.28-2025'},
+            name: 'SosEnt-Norge-GrunnlagsDokument',
+            description: 'Grunnlags Dokument for foreningen.',
+
+            tag: 
+            [
+                'organisasjonen', 'dokumenter', 'medlemskap', 'pdf',
+                'grunnlag', 'cta-dokument'
+            ],
+
+        },
+        {
+            type: 'pdf',
+            label: 'Medlemmer',
+            id: generateHexID(),
+            name: 'SosEnt-Norge-Medlemmer',
+            date: {published: '02.28-2025'},
+            description: 'Liste med medlemmer av foreningen.',
+
+            tag:
+            [
+                'organisasjonen', 'dokumenter', 'medlemskap', 'pdf',
+                'medlemmer'
+            ],
+        },
+    ],
+}
+
 export const fetchMedia = async () =>
 {
     return new Promise(resolve => {
-        setTimeout(() => {resolve(mediaContent);}, 10);
+        setTimeout(() => {resolve(mediaContent.media);}, 10);
     })
 }
