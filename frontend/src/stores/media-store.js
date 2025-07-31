c7f96da58b9803107cfe9bc14361478e8b07fb18
krigjo25 <krigjo25@gmail.com>
2025-07-31 16:46:12 +0200

    Refactor: Enhance media handling logic and structure in media store

--- frontend/src/stores/media-store.js
+++ frontend/src/stores/media-store.js
@@ -1,22 +1,91 @@
 //  This file is a part of the SoSEnT web application project.
-import { reactive } from 'vue';
 import { defineStore } from 'pinia';
 import { fetchMedia } from '@/services/sosent-media-api.js';
 
 export const mediaStore = defineStore('mediaData',
     {
         state: () => ({
+            paths:
+            {
+                images: '/media/images/',
+                documents: '/media/files/documents/',
+            },
             data: {
-                title: 'Media',
-                cls: ['flex-column', 'flex-wrap-row-justify-space-evenly'],
-                items: [],
+                title: 'Dokumenter',
+                media: [],
+                archived: [],
                 isLoaded: false,
             },
         }),
         actions:
         {
+            addMedia(item)
+            {
+                const paths = this.paths;
+                const media = this.data.media;
+
+                switch (item.type)
+                {
+                    case 'pdf':
+                        item.anchor =
+                        {
+                            label: item.label,
+                            type: [item.type, 'external'],
+                            href: paths.documents + item.name + '.' + item.type,
+                        }
+                }
+                if (item.type === 'image')
+                {
+                    item.anchor.href = paths.images + item.anchor.href;
+                }
+                media.push(item);
+                console.warn('Media added:', item);
+            },
+
+            fetchMedia()
+            { 
+                const media = this.data;
+                if (media.isLoaded) return;
+
+                fetchMedia().then((data) =>
+                    {
+                        media.isLoaded = true;
+                        data.forEach((item) => {
+                            this.addMedia(item);
+                        });
+                    }).catch((error) =>
+                    {
+                        this.data.isLoaded = false;
+                        console.error("Error fetching media: ", error);
+                    });
+            },
+            sortMedia()
+            {
+
+            },
+            archiveMedia()
+            {
+                const media = this.data.media;
+                const archive = this.data.archived;
+                media.forEach((item) => {
+                    if (item.tag != 'organization')
+                    {
+                        archive.push(item);
+                    }
+                });
+                media = media.filter(item => !item.archived);
+            }
+
         },
         getters:
         {
+            filter: (state) => (tag) =>
+            {
+                return state.data.media.filter((item) => item.tag.includes(tag));
+            },
+            title: (state) => state.data.title,
+            media: (state) => state.data.media,
+            isLoaded: (state) => state.data.isLoaded,
+            
         },
     });
