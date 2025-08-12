05a88c6b7877fe1f2b1a77a465ae449d33632404
krigjo25 <krigjo25@gmail.com>
2025-08-12 14:22:39 +0200

    Add inputs component for dynamic form handling

---
+++ frontend/src/components/form/inputs.vue
@@ -0,0 +1,38 @@
+<template>
+    <label :for="data.name" :class="cls[0]">{{ data.label }}</label>
+    <input :id="data.id"
+        :class="cls[1]"
+        :placeholder="data.placeholder"
+        :type="data.type ? data.type : 'text'"
+        :size="!!data.size ? data.size : '30'"
+        :width="!!data.width ? data.width : ''"
+        :height="!!data.height ? data.height : ''"
+        :pattern="!!data.pattern ? data.pattern : ''"
+        :readonly="!!data.readonly ? data.readonly : false"
+        :required="!!data.required ? data.required : false"
+        :disabled="!!data.disabled ? data.disabled : false"
+        :maxlength="!!data.maxlength ? data.maxlength : ''"
+        :minlength="!!data.minlength ? data.minlength : ''"
+        :autofocus="!!data.autofocus ? data.autofocus : false"
+        :multiple="!!data.multiple && data.type == 'file' ? data.multiple : false"
+    />
+</template>
+<script setup>
+
+    import { defineProps, ref } from 'vue';
+
+    const props = defineProps({
+        data: {
+            type: Object,
+            required: true
+        },
+        cls: {
+            type: Array,
+            required: false
+        },
+    });
+    const cls = props.cls;
+    const data = props.data;
+
+    console.warn(data);
+</script>
