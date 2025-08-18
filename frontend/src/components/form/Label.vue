c06dae493b2598ae367a52b89475a6b4f70fb465
krigjo25 <krigjo25@gmail.com>
2025-08-18 14:41:45 +0200

    Add Label component: Create a new Label component with props for data and class binding

---
+++ frontend/src/components/form/Label.vue
@@ -0,0 +1,17 @@
+<template>
+    <label :class="cls[0]">{{ data }}</label>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    data: {
+        type: Object,
+        required: true
+    },
+    cls: {
+        type: Array,
+        required: false
+    }
+});
+</script>
