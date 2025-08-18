23f9cd00e4cbaec70af9f766134382e0624ba5ba
krigjo25 <krigjo25@gmail.com>
2025-08-18 14:41:32 +0200

    Fix Datalist component: Update label rendering to conditionally display based on data.id

--- frontend/src/components/form/Datalist.vue
+++ frontend/src/components/form/Datalist.vue
@@ -1,5 +1,6 @@
 <template>
-    <label :for="data.dataList.name">{{ data.dataList.label }}</label>
+    <Label v-if="!!data.id" :for="data.id" 
+    :data="data.label" :cls="cls" />
     <datalist :id="data.dataList.list">
         <option v-for="option in data.dataList.options" :key="option.id" :value="option.value">
             {{ option.label }}
