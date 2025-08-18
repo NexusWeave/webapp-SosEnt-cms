95507d7de9caaff84131a950614839ff03349b05
krigjo25 <krigjo25@gmail.com>
2025-08-18 14:41:56 +0200

    Refactor Select component: Replace label element with Label component for improved structure and consistency

--- frontend/src/components/form/Select.vue
+++ frontend/src/components/form/Select.vue
@@ -1,5 +1,6 @@
 <template>
-    <label :for="selection.label">{{ selection.label }}</label>
+    <Label v-if="!!data.id" :for="data.id" 
+    :data="data.label" :cls="cls" />
     <select v-model="formData.selectedOption" 
         :multiple="selection.multiple ? selection.multiple : false">
         <option v-for="option in data.selectOptions" :key="option.id" :value="option.value">
