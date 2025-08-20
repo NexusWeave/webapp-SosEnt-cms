556167b6411b053ae80bfd11aa3f30dcc0f8aa24
krigjo25 <krigjo25@gmail.com>
2025-08-20 15:11:12 +0200

    Refactor sosent-organization-api.js: Simplify organization data structure and remove unnecessary nesting

--- frontend/src/services/sosent-organization-api.js
+++ frontend/src/services/sosent-organization-api.js
@@ -3,79 +3,67 @@ import {reactive } from 'vue';
 import { generateHexID } from '@/utils/utils';
 
 
-const organization = reactive(
+export const organizationData =
+[
     {
-        team:
+        id: generateHexID(),
+        title: 'Styreleder',
+        name: 'Helle V. Rødahl',
+        contactInfo:
         [
-        {
-            id: generateHexID(),
-            title: 'Styreleder',
-            name: 'Helle V. Rødahl',
-            contactInfo:
-            [
-                {
-                    id: 0,
-                    anchor:
-                    {
-                        href: 'mailto:' + 'helle.' + 'rodahl' +'@' + 'bsn.oslo.kommune.no',
-                    },
-                },
-                {
-                    id: 1,
-                    anchor:
-                    {
-                        href : 'tel:' + '+47' + '41' + '45' + '45' + '00',
-                    },
-                },
-            ],
-
-            img:
             {
-                type: 'jpg',
-                cls: ['card-img'],
-                alt: "JPG bilde av Helle V. Rødahl",
-                src: '/media/images/team/profil-helle-privat.jpg',
+                id: 0,
+                anchor:
+                {
+                    href: 'mailto:' + 'helle.' + 'rodahl' +'@' + 'bsn.oslo.kommune.no',
+                },
             },
-        },
-        {
-            id: generateHexID(),
-            name: 'Rune J. Kvarme',
-            title: 'Generalsekretær',
-
-            contactInfo:
-            [
-                {
-                    id: 0,
-                    anchor:
-                    {
-                        href: 'mailto:' + 'rune.kvarme' + '@' + 'samfunnsbedriftene.no',
-                    },
-                },
-                {
-                    id: 1,
-                    anchor:
-                    {
-                        href : 'tel:' + '+47' + '41' + '54' + '03' + '53',
-                    },
-                    
-                },
-            ],
-
-            img:
             {
-                type: 'png',
-                cls: ['card-img'],
-                alt: "Rune J. Kvarme ",
-                src: '/media/images/team/52700234381_5e75074f92_q.jpg',
+                id: 1,
+                anchor:
+                {
+                    href : 'tel:' + '+47' + '41' + '45' + '45' + '00',
+                },
             },
-        },
         ],
-    });
 
-export const fetchData = async () =>
-{
-    return new Promise(resolve => {
-        setTimeout(() => {
-            resolve(organization);}, 10);
-    })
-}
+        img:
+        {
+            type: 'jpg',
+            cls: ['card-img'],
+            alt: "JPG bilde av Helle V. Rødahl",
+            src: '/media/images/team/profil-helle-privat.jpg',
+        },
+    },
+    {
+        id: generateHexID(),
+        name: 'Rune J. Kvarme',
+        title: 'Generalsekretær',
+        contactInfo:
+        [
+            {
+                id: 0,
+                anchor:
+                {
+                    href: 'mailto:' + 'rune.kvarme' + '@' + 'samfunnsbedriftene.no',
+                },
+            },
+            {
+                id: 1,
+                anchor:
+                {
+                    href : 'tel:' + '+47' + '41' + '54' + '03' + '53',
+                },
+                    
+            },
+        ],
+
+        img:
+        {
+            type: 'png',
+            cls: ['card-img'],
+            alt: "Rune J. Kvarme ",
+            src: '/media/images/team/52700234381_5e75074f92_q.jpg',
+        },
+    },
+];
