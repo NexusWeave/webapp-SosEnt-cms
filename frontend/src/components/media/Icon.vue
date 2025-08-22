65e1ec076fa82b432666cfadec3e1452ae51e5e3
krigjo25 <krigjo25@gmail.com>
2025-08-22 11:54:18 +0200

    Add Icon component: Implement template and props for dynamic icon rendering

---
+++ frontend/src/components/media/Icon.vue
@@ -0,0 +1,28 @@
+<template>
+    <template v-if ="!!label">
+        <span :class="cls[0]">
+        <i :class="cls[cls.length - 1]" :aria-label="label"></i> 
+            {{ label }}
+        </span>
+    </template>
+    <template v-else>
+        <span :class="cls[0]">
+            <i :class="cls[cls.length - 1]" aria-label="icon"></i> 
+        </span>
+    </template>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    label: {
+        type: String,
+        required: false
+    },
+    cls: {
+        type: Array,
+        required: true
+    }
+});
+</script>
