f19f1c61075b624f487c7b9acb8ba5887ebb56a1
krigjo25 <krigjo25@gmail.com>
2025-08-22 14:17:10 +0200

    Refactor Label component: Implement conditional rendering for icon display and enhance label handling

--- frontend/src/components/form/Label.vue
+++ frontend/src/components/form/Label.vue
@@ -1,15 +1,35 @@
 <template>
-    <label>{{ data }}</label>
+    <template v-if="isIcon">
+        <Icon :data="data" :cls="cls" />
+    </template>
+    <template v-else>
+        <label>{{ label }}</label>
+    </template>
 </template>
 <script setup>
-import { defineProps } from 'vue';
+    import { defineProps } from 'vue';
 
-const props = defineProps({
-    data: {
-        type: Object,
-        required: true
-    },
-});
-const data = props.data;
+    import Icon from '../media/Icon.vue';
+
+    const props = defineProps({
+        data: {
+            type: Object,
+            required: true
+        },
+        label: {
+            type: String,
+            required: false
+        },
+        cls: {
+            type: Array,
+            required: false
+        }
+    });
+
+    const cls = props.cls;
+    const data = props.data;
+    const isIcon = !!data;
+    console.log("Label.vue : ", props.label);
+    const label = props.label;
 
 </script>
