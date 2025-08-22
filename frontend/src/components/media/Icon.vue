b2ebf7e6a25045157eb952c6e7d8e16232248866
krigjo25 <krigjo25@gmail.com>
2025-08-22 14:17:13 +0200

    Refactor Icon component: Update conditional rendering for label display and improve props structure

--- frontend/src/components/media/Icon.vue
+++ frontend/src/components/media/Icon.vue
@@ -1,28 +1,49 @@
 <template>
-    <template v-if ="!!label">
+    <template v-if ="!!inputIcon">
         <span :class="cls[0]">
-        <i :class="cls[cls.length - 1]" :aria-label="label"></i> 
+            <i :class="[cls[cls.length - 1], 'relative']" :aria-label="data.label"></i> 
+            <label :for="data.id">{{ data.label }}</label>
+        </span>
+    </template>
+    
+    <template v-else>
+        <span :class="cls[0]">
+            <i :class="cls[cls.length - 1]" aria-label="icon"></i>
             {{ label }}
         </span>
     </template>
-    <template v-else>
-        <span :class="cls[0]">
-            <i :class="cls[cls.length - 1]" aria-label="icon"></i> 
-        </span>
-    </template>
+
 </template>
 
 <script setup>
-import { defineProps } from 'vue';
+    import { computed, defineProps } from 'vue';
 
-const props = defineProps({
-    label: {
-        type: String,
-        required: false
-    },
-    cls: {
-        type: Array,
-        required: true
-    }
-});
+    const props = defineProps(
+        {
+            data:
+            {
+                type: Object,
+                required: true,
+            },
+            label:
+            {
+                type: String,
+                required: false
+            },
+            cls:
+            {
+                type: Array,
+                required: true
+            }
+        });
+
+    const label = props.label;
+    const cls = computed(() => {
+        props.cls.push('icon');
+        return !!props.cls ? props.cls : [];
+    });
+    const inputIcon = computed(() => {
+        if (!!props.data) return true;
+        return false;
+    });
 </script>
