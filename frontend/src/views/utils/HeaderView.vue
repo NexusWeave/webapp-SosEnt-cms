bb0ccfb354bf29be37c7795b219ce9e90e37df90
krigjo25 <krigjo25@gmail.com>
2025-09-04 15:21:07 +0200

    Refactor HeaderView.vue to improve structure by updating class bindings and removing unused props

--- frontend/src/views/utils/HeaderView.vue
+++ frontend/src/views/utils/HeaderView.vue
@@ -1,36 +1,76 @@
 <template>
-    <div :class="cls[0]">
-        <Menu :data="logo" :cls="['logo-container', 'img-list', 'img-item', 'img-container']"/>
-        <Menu :data="menu" :cls="[['nav-bar', 'flex-wrap-row-justify-space-between'],
-                ['nav-list', 'flex-row-align-items-center'],
-                ['nav-item'],
-                ['anchor-item']]"/>
+    <div :class="['flex-wrap-row-align-items-end','component-theme',
+      'logo-container']">
+
+        <Menu :data="images" :cls="['logo-container', 'img-list', 'img-item', 'img-container']"/>
+        <Menu :data="menu"/>
     </div>
-    <section :class="cls[3]">
+    <section class="">
         <h1>{{ content.title }}</h1>
         <p>{{ content.text }}</p>
     </section>
 </template>
 
 <script setup>
-    import { reactive, defineProps } from 'vue';
+    import { generateHexID } from '@/utils/utils.js';
     import Menu from '@/components/navigation/NavMenu.vue';
-    
-    const props = defineProps({
-        data: {
-            type: Object,
-            required: true
-        },
-        cls: {
-            type: Array,
-            required: false,
-        }
-    });
 
-    const cls = props.cls;
-    const data = props.data;
-    const content = data.content;
-    const menu = props.data.menu;
-    const logo = props.data.logo;
+    const content =
+    {
+      title: 'Sosial Entreprenørskap i Norge',
+      text: '- Landsforeningen for Sosiale Entreprenører',
+    };
+
+    const menu =
+      [
+        {
+          href: '/',
+          type: 'router',
+          label: 'Om oss',
+          id: generateHexID(),
+        },
+        {
+          type: 'router',
+          href: '/aktuelt',
+          label: 'Aktuelt og Nyheter',
+          id: generateHexID(),
+        },
+        {
+
+          type: 'router',
+          href: '/fordeler',
+          id: generateHexID(),
+          label: 'Medlemsfordeler',
+        },
+        {
+          type: 'router',
+          id: generateHexID(),
+          href: '/medlemskap',
+          label: 'Medlemskap',
+        },
+      ];
+
+    const images =
+    [
+        {
+            type: 'anchor',
+
+            anchor:
+            {
+                href: '/',
+                id: generateHexID(),
+
+                img:
+                {
+                    type: 'png',
+                    alt: 'SosEnT Logo',
+                    src: '/media/images/logo/SosEnT-logo.png',
+                }
+            }
+        }
+    ]
+    {
+        
+    };
     //console.log("HeaderView loaded with data: ", content, logo);
 </script>
