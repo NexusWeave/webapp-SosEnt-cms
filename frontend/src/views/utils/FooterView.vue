c2544cc2cccb3af409c2e3a8b234eb152fec763b
krigjo25 <krigjo25@gmail.com>
2025-09-04 15:21:20 +0200

    Refactor FooterView.vue to improve structure by simplifying template and removing unused props

--- frontend/src/views/utils/FooterView.vue
+++ frontend/src/views/utils/FooterView.vue
@@ -1,47 +1,81 @@
 <template>
-    <section :class="cls[0]">
-        <section></section>
-        <section>
-        <p>{{ data.copyRight }}</p>
-        </section>
-        <section> 
-            <Menu :data="menu" :cls="[['nav-bar', 'flex-wrap-row-justify-flex-end'],
+    <section class="flex-column component-theme
+">
+    <section :class="['flex-wrap-row', 'component-theme',
+      'flex-wrap-row-justify-center']">
+        <address>
+            <p v-for="addr in content.address" :key="addr.postal">
+                {{ addr.msg }} {{ addr.address }}, {{ addr.postal }} <br/>
+            </p>
+
+        </address>
+        <section class="flex-column"> 
+            <Menu :data="menu" :cls="[['nav-bar', 'flex-column'],
                 ['nav-list'],
                 ['nav-item'],
-                ['anchor-item']]"/>
-            <p>Besøksadresse: Haakon VIIs gate 9. 0161 Oslo</p>
-            <p>Postadresse: co/ Samfunnsbedriftene, postboks 1378 Vika,0114 Oslo</p>
+                ['nav-link']]"/>
         </section>
     </section>
+    <p>{{ content.copyRight }}</p>
     
-    
+    </section>
 </template>
 
 <script setup>
 
-    import { defineProps } from 'vue';
+    import { generateHexID } from '@/utils/utils.js';
     import Menu from '@/components/navigation/NavMenu.vue';
 
-    const props = defineProps({
-        data:
+  const menu =
+    [
         {
-            type: Object,
-            required: true
-        },
-        cls:
-        {
-            type: [String, Array],
-            required: true,
-        },
-        menu: {
-            type: Array,
-            required: true,
-        }
-    });
+            type: 'anchor',
+            id: generateHexID(),
 
-    const cls = props.cls;
-    const data = props.data;
-    const menu = props.menu;
+            anchor:
+            {
+                type: ['external'],
+                label: "Personvern",
+                href: '/media/files/documents/SosEnT-Norge-Personvern.pdf',
+                
+            },
+        },
+        {
+            type: 'anchor',    
+            id: generateHexID(),
+
+            anchor:
+            {
+                type: ['external'],
+                label: " Org nr : (935 329 930)",
+                href: 'https://proff.no/selskap/sosent-norge/oslo/medlemsorganisasjoner/IFGVD6Y10PU',
+            },
+        }
+    ];
+
+
+  const content = 
+  {
+      copyRight: ' © alle rettigheter reservert av SosEnT Norge',
+      address:
+      [
+        {
+            postal: "0161 Oslo",
+            msg: "Besøk oss i Oslo:",
+            address: "Haakon VIIs gate 9."
+        },
+        {
+            postal: "0114 Oslo",
+            msg:"Send oss post til:",
+            address: "co/ Samfunnsbedriftene, postboks 1378."
+        }
+      ],
+      contact:
+        {
+            email: 'post@sosent.no',
+            phone: '+47 22 20 88 00'
+        }
+    };
 
     //console.log("FooterView loaded with data: ", data);
 </script>
